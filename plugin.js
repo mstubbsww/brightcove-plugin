22e1185a68e40d0be412cd1e27affa980c04ce48
mstubbsww <52669600+mstubbsww@users.noreply.github.com>
2019-07-08 11:44:30 -0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,6 +1,3 @@
-//videojs.getAllPlayers()[0].ready(function() { 
-//  var myPlayer = this;
-//  myPlayer.on('play',function(evt,data){
-//    window.parent.postMessage("working",  "https://localhost:5201"); 
-//  });
-//});
+videojs.getPlayer().on('play',function(evt,data){  
+  window.parent.postMessage("working",  "https://localhost:5201"); 
+});
