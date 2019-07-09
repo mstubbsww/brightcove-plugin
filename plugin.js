aa0a16d87a8b4aec9c1792c44a114c01ed9d0223
mstubbsww <52669600+mstubbsww@users.noreply.github.com>
2019-07-08 23:33:38 -0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,7 +1,7 @@
 window.onload = function(e) {
   videojs.getPlayer("vjs_video_3").ready(function() {
-    this.on("play",function(evt,data) {  
-      window.parent.postMessage("working",  "https://localhost:5201"); 
+    this.on("play",function(e) {  
+      window.parent.postMessage(this.mediainfo.id,  "https://localhost:5201"); 
     });
   });
 };
